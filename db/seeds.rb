795dda059f487b90590cefe5ab2b9dd7dc5197b1
James Yeates <jimibue@gmail.com>
2020-08-27 10:47:29 -0700

    some views

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# user1 = User.create(first_name:'BOb', last_name:'dolez')
+require "faker"
+roles = ["teacher", "ta", "student"]
+10.times do
+  course = Course.create(
+    name: Faker::Educator.course_name,
+  )
+
+  10.times do
+    user = User.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+    )
+
+    Enrollment.create(
+      role: roles.sample,
+      course_id: course.id,
+      user_id: user.id,
+    )
+  end
+end
+
+puts "Data Seeded."
 
 # course1 = Course.create(name:'Science')
 
