8e8cfa2ff7d57b8bd1adac19271eebd27089a6a8
James Yeates <jimibue@gmail.com>
2020-08-27 13:43:24 -0700

    demo done

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,6 +1,7 @@
 class EnrollmentsController < ApplicationController
+  before_action :set_course
+
   def index
-    @course = Course.find(params[:course_id])
     @teachers = @course.enrollments.where(role: "teacher")
     @students = @course.enrollments.where(role: "student")
     @tas = @course.enrollments.where(role: "ta")
@@ -8,5 +9,34 @@ class EnrollmentsController < ApplicationController
   end
 
   def new
+    # users that are not enrolled in the active course
+    # which fin params[:course_id]
+    @enrollment = @course.enrollments.new
+    @users = User.all - @course.users
+  end
+
+  def create
+    @enrollment = @course.enrollments.new(enrollment_params)
+    if (@enrollment.save)
+      redirect_to course_enrollments_path(@course)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @enrollment = @course.enrollments.find(params[:id])
+    @enrollment.destroy
+    redirect_to course_enrollments_path(@course)
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
+
+  def enrollment_params
+    params.require(:enrollment).permit(:role, :user_id)
   end
 end
